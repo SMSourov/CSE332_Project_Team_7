2aace687219ba55a37ff63ff24a061ee35e03c53
SMSourov <81343486+SMSourov@users.noreply.github.com>
2022-08-26 23:11:28 +0600

    Add files via upload

---
+++ Assembler/src/assembler/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assembler;
+
+/**
+ *
+ * @author Sk. Mashrur Sourov
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Assembler a = new Assembler();
+        a.setVisible(true);
+    }
+    
+}
